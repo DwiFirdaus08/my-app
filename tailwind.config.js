cdd42dcfa4ac7e5007256bcb654a8feb0e537721
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2025-04-14 13:27:23 +0700

    fixed|| js to jsx in tailwild.config.js
    added||input.css and inpoer output,css in main.jsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,24 @@
-import withMT from "@material-tailwind/react/utils/withMT.js";
+// import withMT from "@material-tailwind/react/utils/withMT.js";
+
+// /** @type {import('tailwindcss').Config} */
+// export default withMT({
+//   content: [
+//     "./index.html",
+//     "./src//*.{vue,js,ts,jsx,tsx}",
+//     "node_modules/@material-tailwind/react/components//*.{js,ts,jsx,tsx}",
+//     "node_modules/@material-tailwind/react/theme/components//*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// });
 
 /** @type {import('tailwindcss').Config} */
-export default withMT({
-  content: [
-    "./index.html",
-    "./src//*.{vue,js,ts,jsx,tsx}",
-    "node_modules/@material-tailwind/react/components//*.{js,ts,jsx,tsx}",
-    "node_modules/@material-tailwind/react/theme/components//*.{js,ts,jsx,tsx}",
-  ],
+export default {
+  content: ["./src/**/*.{html,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-});
+};
